3d370d2c8f767ad53c1cd18a11f21ac64d27db70
e534e71 <e534e71@gmail.com>
2023-01-18 12:02:15 +0300

    Comment added

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,5 @@
-﻿global using System;
+﻿// System
+global using System;
 global using System.Linq;
 global using System.Text;
 global using System.Text.Json;
@@ -6,12 +7,14 @@ global using System.Threading.Tasks;
 global using System.Collections.Generic;
 global using System.Collections.ObjectModel;
 
+//Files
 global using Lesson.View;
 global using Lesson.Model;
 global using Lesson.ViewModel;
 
 global using Lesson.Services;
 
+// Tools
 global using Newtonsoft.Json;
 
 global using CommunityToolkit.Mvvm.Input;
